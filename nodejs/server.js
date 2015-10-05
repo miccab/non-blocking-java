b58dfde8b2432affe1113c6bc7b446fb5f80e42c
Michal <michal@world.com>
2015-10-05 23:59:28 +0200

    increase pool size

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,6 +2,7 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 var pg = require('pg');
+pg.defaults.poolSize = 100;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
