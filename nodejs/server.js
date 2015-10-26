3400088a43432784275dd397e12881d34ea817a2
Michal <michal@world.com>
2015-10-26 06:49:42 +0100

    configure large db pool to node server

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,7 +2,7 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 var pg = require('pg');
-pg.defaults.poolSize = 32;
+pg.defaults.poolSize = 500;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
